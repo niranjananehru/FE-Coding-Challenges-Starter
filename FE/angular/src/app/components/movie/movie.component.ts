3c1ee3d810f1b52b09f3ad081a597325c057fff4
niranjananehru <niranj22@gmail.com>
2023-08-24 13:41:05 +0000

    Movie Details Bug Repair

--- FE/angular/src/app/components/movie/movie.component.ts
+++ FE/angular/src/app/components/movie/movie.component.ts
@@ -8,15 +8,19 @@ import { DataService, MovieComplete } from '../../services/data.service';
   templateUrl: './movie.component.html'
 })
 export class MovieComponent implements OnDestroy, OnInit {
-  public movie: MovieComplete;
-  public movieId = '';
+  public movie: MovieComplete | undefined;
+  public movieId : string | null = '';
   private movieSubscription: any;
 
   constructor(private activatedRoute: ActivatedRoute, private dataService: DataService) {}
 
   public ngOnInit() {
-    this.activatedRoute.params.pipe(tap(({ id }) => (this.movieId = id)));
-    this.movieSubscription = this.dataService.getMovie(this.movieId).pipe(tap((data) => (this.movie = data)));
+    this.movieId = this.activatedRoute.snapshot.paramMap.get('id');
+    this.movieSubscription = this.dataService.getMovies().pipe(
+      tap(movies => { 
+        this.movie = movies.Search.find(srch => srch.imdbID === this.movieId);
+      })
+    ).subscribe();
   }
 
   public ngOnDestroy(): void {
